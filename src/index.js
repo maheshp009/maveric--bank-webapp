2cfa5f672a6f354f71b6a89d29d9ebe499863358
Palla Mahesh <pallam@maveric-systems.com>
2022-05-24 12:00:48 +0530

    initial boiler plate code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import'./Components/Pages/Login';
+import Login from'./Components/Pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Login />
     <App />
   </React.StrictMode>
 );
